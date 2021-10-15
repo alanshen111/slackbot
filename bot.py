a3a8b98a9db6030b13af52e6d65c1b3b4d09eac0
Alan Shen <alanshen111@gmail.com>
2021-10-14 21:04:17 -0700

    hi

--- bot.py
+++ bot.py
@@ -6,4 +6,6 @@ from dotenv import load_dotenv
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
 
-client = slack.WebClient(token=os.environ)['SLACK_TOKEN']
+client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
+
+client.chat_postMessage(channel='#test', text = "hi")
